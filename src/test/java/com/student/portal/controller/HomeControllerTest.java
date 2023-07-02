1cba3a8b190dd7840f3cd2dafad1b471d53bef72
olafasasi <ola.fasasi@gmail.com>
2023-07-02 12:57:48 +0300

    HomeControllerTest loginWrongRequestTest added

--- src/test/java/com/student/portal/controller/HomeControllerTest.java
+++ src/test/java/com/student/portal/controller/HomeControllerTest.java
@@ -1,15 +1,17 @@
 package com.student.portal.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.student.portal.dao.dto.StudentDto;
 import com.student.portal.dao.entities.Student;
+import com.student.portal.dao.repository.StudentRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.context.ActiveProfiles;
@@ -18,6 +20,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -31,7 +34,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class HomeControllerTest {
 
     private Student student;
-    private StudentDto studentDto;
     private final String email = "test@email.com";
     private final String role = "ROLE_STUDENT";
     private final Long id = 1L;
@@ -40,17 +42,26 @@ public class HomeControllerTest {
     private static final String REGISTER = "/register";
     private static final String LOGOUT_PATH = "/login";
     private static final String LOGOUT_PATH_REDIRECT = "/login?logout";
-
-    private ObjectMapper objectMapper = new ObjectMapper();
     @Autowired
     private WebApplicationContext wac;
     @Autowired
     private FilterChainProxy filterChain;
     @Autowired
     private MockMvc mockMvc;
+    @MockBean
+    private StudentRepository studentRepository;
 
     @BeforeEach
     public void setUp() {
+        this.student = new Student();
+        this.student.setId(this.id);
+        this.student.setFirstName("ALFA");
+        this.student.setLastName("BETA");
+        this.student.setEmail(this.email);
+        this.student.setPasswordHash("$2a$10$OtjiCN27cKN9CY6zvV7w7OHKDULtiJxgQFo3.zKC8j0xFCnWLwsfq");
+        this.student.setRole(this.role);
+        Mockito.when(this.studentRepository.findByEmail(this.student.getEmail())).thenReturn(this.student);
+        Mockito.when(this.studentRepository.save(this.student)).thenReturn(this.student);
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).addFilter(filterChain).build();
     }
 
@@ -101,5 +112,13 @@ public class HomeControllerTest {
         .andExpect(content().string(containsString("Login")))
         .andExpect(content().string(containsString("username")))
         .andExpect(content().string(containsString("password")));
+    }
+
+    @Test
+    public void loginWrongRequestTest() throws Exception {
+        this.mockMvc.perform(formLogin(LOGOUT_PATH)
+        .user("username", "test@email.com").password("password","B@234"))
+        .andExpect(status().isFound())
+        .andExpect(redirectedUrl("/login?error"));
     }
 }
